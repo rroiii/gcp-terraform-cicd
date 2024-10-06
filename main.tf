e8c6e1bf93cc344e09d47514979c3c2700d9f1c8
rroiii <royoswaldha@gmail.com>
2024-10-06 19:01:45 +0700

    Add project

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "terraform_bucket" {
   name                     = "terraform-bucket"
+  project                  = "high-sunlight-437501-f6"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
