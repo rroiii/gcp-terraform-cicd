ee41931afd62834e33bfe5ab2f41d1f3eb5e5f6d
rroiii <royoswaldha@gmail.com>
2024-10-06 19:28:32 +0700

    Change bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "terraform_bucket" {
-  name                     = "terraform-bucket"
+  name                     = "skripsi-test-bucket"
   project                  = "high-sunlight-437501-f6"
   location                 = "US"
   force_destroy            = true
