96745d00ea08a275eeec51744cfae15b54b78a31
rroiii <royoswaldha@gmail.com>
2024-10-07 11:33:42 +0700

    public access prevention set to enforced

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "terraform_bucket" {
-  name          = "skripsi-test-bucket"
-  project       = "high-sunlight-437501-f6"
-  location      = "US"
-  force_destroy = true
+  name                     = "skripsi-test-bucket"
+  project                  = "high-sunlight-437501-f6"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
 }
