13c8b7601e139fa154b3d7702d8f93d13469c3cc
rroiii <royoswaldha@gmail.com>
2024-10-07 11:47:13 +0700

    Public not enforced

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "terraform_bucket" {
-  name                     = "skripsi-test-bucket"
-  project                  = "high-sunlight-437501-f6"
-  location                 = "US"
-  force_destroy            = true
+  name          = "skripsi-test-bucket"
+  project       = "high-sunlight-437501-f6"
+  location      = "US"
+  force_destroy = true
 }
